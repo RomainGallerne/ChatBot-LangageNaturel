7096545800922b37c18f61f219c881fa0367dba5
Balerion14 <93737471+Balerion14@users.noreply.github.com>
2024-03-06 08:34:36 +0100

    Update webscraper.py
    aout code dans une fonction et sauvegarder dans un txt

--- webscraper.py
+++ webscraper.py
@@ -2,11 +2,25 @@ import requests
 
 url = "https://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=tigre&rel="  # Remplacez ceci par l'URL de la page que vous souhaitez récupérer
 
-# Envoi de la requête HTTP GET pour récupérer la page
-response = requests.get(url)
+def get_data(file: str, test: bool = False):
+    """
+    Fonction qui va sauvegarder fichier dans un .txt
+    @param file: str        
+    """
+    # Déclaration de la variable globale url
+    global url
+    # Envoi de la requête HTTP GET pour récupérer la page
+    try:
+        response = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        print("Erreur lors de la récupération de la page :", e)
+    else: 
+        # Sauvegarde du contenu de la page dans un fichier
+        with open(file, "w", encoding="utf-8") as f:  # Spécification de l'encodage utf-8
+            f.write(response.text)
+    # Si test est vrai, on affiche le contenu de la page
+    if test:
+        print(response.text)
 
-# Vérification si la requête a réussi (code de statut 200)
-if response.status_code != 200: #La requête a échoué
-    print("Erreur lors de la récupération de la page :", response.status_code)
-else: #La requête a réussi
-    print(response.text)
+if __name__ == "__main__":
+    get_data("data.txt", True)  # On sauvegarde le contenu de la page dans un fichier
